729055f10fa6b708651a13873ec7a37f642f96c8
Christian Korndörfer <info@chko.org>
2014-05-28 01:22:45 +0200

    basic set up

---
+++ app/database/migrations/2014_05_27_220934_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function($table)
+		{
+		    $table->increments('id');
+		    $table->string('email', 50);
+		    $table->string('user_name', 20);
+		    $table->string('password', 60);
+		    $table->string('password_temp', 60);
+		    $table->string('code', 60);
+		    $table->boolean('active');
+		    $table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('users');
+	}
+
+}
