d3514ce386115c48a28073f166895f74c01d9efa
Christian Korndörfer <info@chko.org>
2014-05-30 03:01:51 +0200

    basic bootstrap templating

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -21,7 +21,7 @@ class UsersController extends \BaseController {
 	 */
 	public function create()
 	{
-		//
+		return View::make('users.create');
 	}
 
 	/**
--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -1,1 +1,36 @@
-/Users/christiankorndoerfer/Projekte/diskollect/app/views/users/create.blade.php
+@extends('layout.main')
+
+@section('title')
+  Registration
+@stop
+
+@section('body')
+  <div class="page-header">
+    <h1>Registration</h1>
+  </div>
+
+  <div class="form-wrapper">
+    <form role="form">
+      <div class="form-group">
+        <label for="exampleInputEmail1">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputPassword1">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+      </div>
+      <div class="form-group">
+        <label for="exampleInputFile">File input</label>
+        <input type="file" id="exampleInputFile">
+        <p class="help-block">Example block-level help text here.</p>
+      </div>
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Check me out
+        </label>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+  </div>
+@stop
+
