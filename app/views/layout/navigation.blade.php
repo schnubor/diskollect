01fefc4483fd3bf2ce0f30a9a3bc10dcac5c1bcd
schnubor <info@chko.org>
2014-06-12 21:05:30 +0200

    nav update

--- app/views/layout/navigation.blade.php
+++ app/views/layout/navigation.blade.php
@@ -8,7 +8,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="{{ URL::to('/') }}">Diskollect</a>
+      <a class="navbar-brand" href="{{ URL::to('/') }}" style="font-weight: bold;">Diskollect</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
@@ -26,7 +26,7 @@
           <li class="dropdown">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->username }}<b class="caret"></b></a>
             <ul class="dropdown-menu">
-              <li><a href="{{ URL::to('users/edit') }}"><i class="fa fa-gears fa-fw"></i> Settings</a></li>
+              <li><a href="{{ URL::to('users/edit') }}"><i class="fa fa-gear fa-fw"></i> Settings</a></li>
               <li><a href="{{ URL::to('users') }}/{{ Auth::user()->id }}"><i class="fa fa-user fa-fw"></i> Your Profile</a></li>
               <li class="divider"></li>
               <li><a href="{{ URL::to('users/change-password') }}"><i class="fa fa-lock fa-fw"></i> Change password</a></li>
