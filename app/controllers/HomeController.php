8298afb228627400b19e6d12529a3375c7348e91
Christian Korndörfer <info@chko.org>
2014-06-20 03:20:21 +0200

    ouath on page load

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -17,12 +17,47 @@ class HomeController extends BaseController {
 
 	public function showWelcome()
 	{
+		// oAuth to Discogs to enable services
+		$client = new oauth_client_class;
+		$client->debug = false;
+        $client->debug_http = true;
+        $client->server = 'Discogs';
+        $client->redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].
+            dirname(strtok($_SERVER['REQUEST_URI'],'?'));
 
-		/*Mail::send('emails.auth.test', array('name' => 'Chris'), function($message){
-			$message->to('schnuppser@gmail.com', 'Christian Korndörfer')->subject('Test email');
-		});*/
+        $client->client_id = 'iLHTUYXHgeToxDaAWpAX'; $application_line = __LINE__;
+        $client->client_secret = 'dPaSaHBIyyNgNqZuPFPpBTCentqzPxPj';
 
-		return View::make('welcome');
-	}
+        if(strlen($client->client_id) == 0
+        || strlen($client->client_secret) == 0)
+            die('Please go to Discogs Developers page http://www.discogs.com/applications/edit , '.
+                'create an application, and in the line '.$application_line.
+                ' set the client_id to Consumer key and client_secret with Consumer secret. '.
+                'The Callback URL must be '.$client->redirect_uri);
+
+        return View::make('welcome');
+
+        if(($success = $client->Initialize()))
+        {
+            if(($success = $client->Process()))
+            {
+                if(strlen($client->access_token))
+                {
+                    $success = $client->CallAPI(
+                        'http://api.discogs.com/oauth/identity', 
+                        'GET', array(), array('FailOnAccessError'=>true), $user);
+                }
+            }
+            $success = $client->Finalize($success);
+        }
+        
+        if($client->exit)
+            exit;
+
+        if($success)
+        {
+			return View::make('welcome');
+		}
+    }
 
 }
