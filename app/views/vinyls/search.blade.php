04178e04575720229a0fa2e45da21cbcc8ee7afc
schnubor <info@chko.org>
2014-07-05 21:49:09 +0200

    style fix

--- app/views/vinyls/search.blade.php
+++ app/views/vinyls/search.blade.php
@@ -10,32 +10,34 @@
   </div>
   
   <div class="row">
-    <div class="col-md-3 well">
-      <div class="form-wrapper">
-        {{ Form::open(array('url' => 'search')) }}
-          <div class="form-group">
-            {{ Form::label('artist', 'Artist') }}
-            {{ Form::text('artist', Input::old('artist'), array('class' => 'form-control')); }}
+    <div class="col-md-3">
+      <div class="well">
+        <div class="form-wrapper">
+          {{ Form::open(array('url' => 'search')) }}
+            <div class="form-group">
+              {{ Form::label('artist', 'Artist') }}
+              {{ Form::text('artist', Input::old('artist'), array('class' => 'form-control')); }}
 
-            @if($errors->has('artist'))
-              <div class="alert alert-danger">
-                {{ $errors->first('artist') }}
-              </div>
-            @endif
-          </div>
-          <div class="form-group">
-            {{ Form::label('title', 'Title') }}
-            {{ Form::text('title', Input::old('title'), array('class' => 'form-control')); }}
+              @if($errors->has('artist'))
+                <div class="alert alert-danger">
+                  {{ $errors->first('artist') }}
+                </div>
+              @endif
+            </div>
+            <div class="form-group">
+              {{ Form::label('title', 'Title') }}
+              {{ Form::text('title', Input::old('title'), array('class' => 'form-control')); }}
 
-            @if($errors->has('title'))
-              <div class="alert alert-danger">
-                {{ $errors->first('title') }}
-              </div>
-            @endif
-          </div>
+              @if($errors->has('title'))
+                <div class="alert alert-danger">
+                  {{ $errors->first('title') }}
+                </div>
+              @endif
+            </div>
 
-          {{ Form::submit('Search', array('class' => 'btn btn-primary')) }}
-        {{ Form::close() }}
+            {{ Form::submit('Search', array('class' => 'btn btn-primary')) }}
+          {{ Form::close() }}
+        </div>
       </div>
     </div>
   </div>
