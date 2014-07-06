0f34244abb51a3922a8eff50d9443b9607c7a0ba
Christian Korndörfer <info@chko.org>
2014-07-06 23:18:01 +0200

    new vinyl detail page

--- app/views/vinyls/show.blade.php
+++ app/views/vinyls/show.blade.php
@@ -13,70 +13,53 @@
 
   @if(Auth::check())
     <a class="btn btn-default btn-sm" role="button" href="{{ URL::to('users') }}/{{ Auth::user()->id }}/collection"><i class="fa fa-angle-double-left"></i> Back to collection</a>
-
+    @if($vinyl->user_id == Auth::user()->id)
+      <div class="btn btn-success pull-right disabled">{{ round($vinyl->price, 2) }} EUR</div>
+    @endif
     <hr>
   @endif
 
   <div class="row">
-    <div class="col-md-3">
-      <div class="thumbnail">
-        <img src="{{ $vinyl->artwork }}" alt="{{ $vinyl->artist . ' - ' . $vinyl->title }}">
-      </div>
-    </div>
-
-    <div class="col-md-6">
+    <div class="col-md-4 col-md-offset-4">
       <div class="row">
-        <div class="col-md-12">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title">{{ $vinyl->artist }} - {{ $vinyl->title }} <small>released {{ $vinyl->releasedate }}</small></h3>
-            </div>
-            <div class="panel-body">
-              <div class="row">
-                <div class="col-md-12">
-                  @foreach($labels as $label)
-                    <span class="label-primary label">{{ $label }}</span>
-                  @endforeach
-                  @foreach($genres as $genre)
-                    <span class="label-warning label">{{ $genre }}</span>
-                  @endforeach
-                  <span class="label-success label">{{ $vinyl->country }}</span>
-                  <span class="label-default label">{{ $vinyl->count . 'x ' . $vinyl->size . '" ' . $vinyl->type }}</span>
-                </div>
-              </div>
-            </div>
+        <div class="col-md-10 col-md-offset-1">
+          <div class="thumbnail">
+            <img src="{{ $vinyl->artwork }}" alt="{{ $vinyl->artist . ' - ' . $vinyl->title }}">
           </div>
         </div>
-
-        <div class="col-md-4">
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              <h3 class="panel-title">Price</h3>
-            </div>
-            <div class="panel-body">
-              <p class="vinyl-detail price">
-                {{ round($vinyl->price, 2) }} EUR
-              </p>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
-  
-    @if($vinyl->notes)
-      <div class="col-md-3">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">Notes <small>by {{ User::find($vinyl->user_id)->username }}</small></h3>
-          </div>
-          <div class="panel-body">
-            {{ $vinyl->notes }}
-          </div>
-        </div>
-      </div>
-    @endif
   </div>
+
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h1 class="vinyl-detail vinyl-title">{{ $vinyl->artist }} - {{ $vinyl->title }}</h1>
+    </div>
+    <div class="col-md-12 text-center">
+      @foreach($labels as $label)
+        <span class="label-primary label">{{ $label }}</span>
+      @endforeach
+      @foreach($genres as $genre)
+        <span class="label-warning label">{{ $genre }}</span>
+      @endforeach
+      <span class="label-success label">{{ $vinyl->country }}</span>
+      <span class="label-default label">{{ $vinyl->count . 'x ' . $vinyl->size . '"'}}</span>
+      <span class="label-default label">{{ $vinyl->releasedate }}</span>
+    </div>
+  </div>
+
+  <hr>
+
+  <div class="row">
+    <div class="col-md-12">
+      @if($vinyl->notes)
+        <span class="vinyl-detail notes">"{{ $vinyl->notes }}", </span>
+      @endif
+      <span>added {{ $vinyl->created_at }}</span>
+    </div>
+  </div>
+
+  <hr>
 
   <div class="row">
 
@@ -84,7 +67,7 @@
     <div class="col-md-3">
       <div class="panel panel-default">
         <div class="panel-heading">
-          <h3 class="panel-title">Added by</h3>
+          <h3 class="panel-title">Owned by</h3>
         </div>
         
         <img width="100%" src="{{ User::find($vinyl->user_id)->image }}" alt="{{ User::find($vinyl->user_id)->username }}">
