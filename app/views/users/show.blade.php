659e267c90d192b464050cc56fefddb0983f03e5
Christian Korndoerfer <korndoerfer@tape.tv>
2014-07-09 11:08:05 +0200

    view update

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -26,6 +26,8 @@
         @endif
       </ul>
 
+      <a href="{{ URL::route('get-collection', $user->id) }}" class="btn btn-lg btn-primary" style="width: 100%; margin-bottom: 20px;"><i class="fa fa-fw fa-database"></i> View Collection</a>
+
       @if($user->website)
         <div class="panel panel-default">
           <div class="panel-heading">Website <i class="fa fa-link fa-1x"></i></div>
@@ -39,16 +41,15 @@
         <div class="panel-body">{{ $user->description }}</div>
       </div>
       @endif
+
+      @if(Auth::check() && Auth::user()->id == $user->id)
+        <a href="{{ URL::to('users/edit') }}" class="btn btn-default btn-sm" role="button" style="margin-right: 10px;"><i class="fa fa-gear fa-fw"></i> Edit Profile</a>
+      @endif
     </div>
 
     <div class="col-sm-9">
       <div class="page-header profile-user-name">
-        <h1>{{ $user->username }} <small>{{ $user->vinyls->count() }} vinyls</small>
-        <a href="{{ URL::to('users/edit') }}" class="btn btn-primary btn-sm pull-right" role="button"><i class="fa fa-database fa-fw"></i> View Collection</a>
-        @if(Auth::check() && Auth::user()->id == $user->id)
-          <a href="{{ URL::to('users/edit') }}" class="btn btn-default btn-sm pull-right" role="button" style="margin-right: 10px;"><i class="fa fa-gear fa-fw"></i> Edit Profile</a>
-        @endif
-        </h1>
+        <h1>{{ $user->username }} <small>{{ $user->vinyls->count() }} vinyls</small></h1>
       </div>
 
       <!-- Level progress -->
@@ -59,18 +60,12 @@
         </div>
       </div>
       <div class="row">
-        <div class="col-md-1">
-          <strong>Lvl {{ $level }}</strong>
-        </div>
-        <div class="col-md-10">
+        <div class="col-md-12">
           <div class="progress">
             <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="{{ $progress }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress }}%;">
               {{ $progress }}%
             </div>
           </div>
-        </div>
-        <div class="col-md-1">
-          <strong class="pull-right">Lvl {{ $level+1 }}</strong>
         </div>
       </div>
 
