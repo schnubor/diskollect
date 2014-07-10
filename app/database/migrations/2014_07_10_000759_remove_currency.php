6ad4651d3d7a63d1b1838d4b90d556eb627425f6
Christian Korndörfer <info@chko.org>
2014-07-10 02:08:57 +0200

    added currency

---
+++ app/database/migrations/2014_07_10_000759_remove_currency.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class RemoveCurrency extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table){
+			$table->dropColumn('currency');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
--- app/views/users/edit.blade.php
+++ app/views/users/edit.blade.php
@@ -5,65 +5,94 @@
 @stop
 
 @section('body')
-  <div class="page-header">
-    <h1>Edit Profile</h1>
-  </div>
 
-  <div class="form-wrapper">
-    {{ Form::model($user, array('route' => array('update-user', $user->id), 'method' => 'PUT', 'files' => true)) }}
-      <div class="form-group">
-        {{ Form::label('profilepic', 'Profile Picture') }}
-        {{ Form::file('profilepic', array('class' => 'form-control')); }}
+  <div class="row">
+    <div class="col-md-6 col-md-offset-3 well">
+      <legend>Edit Profile</legend>
+      <div class="form-wrapper">
+        {{ Form::model($user, array('route' => array('update-user', $user->id), 'method' => 'PUT', 'files' => true, 'class' => 'form-horizontal')) }}
+          <div class="form-group">
+            {{ Form::label('profilepic', 'Profile Picture', array('class' => 'col-sm-3 control-label')) }}
+            <div class="col-sm-9">
+              {{ Form::file('profilepic', array('class' => 'form-control')); }}
+            </div>
 
-        @if($errors->has('profilepic'))
-          <div class="alert alert-danger">
-            {{ $errors->first('profilepic') }}
+            @if($errors->has('profilepic'))
+              <div class="alert alert-danger">
+                {{ $errors->first('profilepic') }}
+              </div>
+            @endif
           </div>
-        @endif
-      </div>
-      <div class="form-group">
-        {{ Form::label('name', 'Name') }}
-        {{ Form::text('name', Input::old('name'), array('class' => 'form-control', 'placeholder' => 'Your real name')); }}
 
-        @if($errors->has('name'))
-          <div class="alert alert-danger">
-            {{ $errors->first('name') }}
+          <div class="form-group">
+            {{ Form::label('name', 'Name', array('class' => 'col-sm-3 control-label')) }}
+            <div class="col-sm-9">
+              {{ Form::text('name', Input::old('name'), array('class' => 'form-control', 'placeholder' => 'Your real name')); }}
+            </div>
+
+            @if($errors->has('name'))
+              <div class="alert alert-danger">
+                {{ $errors->first('name') }}
+              </div>
+            @endif
           </div>
-        @endif
-      </div>
-      <div class="form-group">
-        {{ Form::label('location', 'Location') }}
-        {{ Form::text('location', Input::old('location'), array('class' => 'form-control', 'placeholder' => 'Your current location')); }}
 
-        @if($errors->has('location'))
-          <div class="alert alert-danger">
-            {{ $errors->first('location') }}
+          <div class="form-group">
+            {{ Form::label('location', 'Location', array('class' => 'col-sm-3 control-label')) }}
+            <div class="col-sm-9">
+              {{ Form::text('location', Input::old('location'), array('class' => 'form-control', 'placeholder' => 'Your current location')); }}
+            </div>
+
+            @if($errors->has('location'))
+              <div class="alert alert-danger">
+                {{ $errors->first('location') }}
+              </div>
+            @endif
           </div>
-        @endif
-      </div>
-      <div class="form-group">
-        {{ Form::label('website', 'Website') }}
-        {{ Form::text('website', Input::old('website'), array('class' => 'form-control', 'placeholder' => 'http://www.example.com')); }}
 
-        @if($errors->has('website'))
-          <div class="alert alert-danger">
-            {{ $errors->first('website') }}
+          <div class="form-group">
+            {{ Form::label('website', 'Website', array('class' => 'col-sm-3 control-label')) }}
+            <div class="col-sm-9">
+              {{ Form::text('website', Input::old('website'), array('class' => 'form-control', 'placeholder' => 'http://www.example.com')); }}
+            </div>
+
+            @if($errors->has('website'))
+              <div class="alert alert-danger">
+                {{ $errors->first('website') }}
+              </div>
+            @endif
           </div>
-        @endif
-      </div>
-      <div class="form-group">
-        {{ Form::label('description', 'Description') }}
-        {{ Form::text('description', Input::old('description'), array('class' => 'form-control', 'placeholder' => '140 characters description')); }}
 
-        @if($errors->has('description'))
-          <div class="alert alert-danger">
-            {{ $errors->first('description') }}
+          <div class="form-group">
+            {{ Form::label('description', 'Description', array('class' => 'col-sm-3 control-label')) }}
+            <div class="col-sm-9">
+              {{ Form::text('description', Input::old('description'), array('class' => 'form-control', 'placeholder' => '140 characters description')); }}
+            </div>
+
+            @if($errors->has('description'))
+              <div class="alert alert-danger">
+                {{ $errors->first('description') }}
+              </div>
+            @endif
           </div>
-        @endif
-      </div>
 
-      {{ Form::submit('Update', array('class' => 'btn btn-primary')) }}
-    {{ Form::close() }}
-  </div>
+          <div class="form-group">
+            {{ Form::label('currency', 'Currency', array('class' => 'col-sm-3 control-label')) }}
+            <div class="col-sm-9">
+              {{ Form::select('currency', array('EUR' => '&euro; - Euro', 'USD' => '&#36; - United States Dollar', 'GBP' => '&pound; - Great Britain Pound'), 'EUR', array('class' => 'form-control')) }}
+            </div>
+
+            @if($errors->has('description'))
+              <div class="alert alert-danger">
+                {{ $errors->first('description') }}
+              </div>
+            @endif
+          </div>
+
+          {{ Form::submit('Update Profile', array('class' => 'btn btn-primary pull-right')) }}
+        {{ Form::close() }}
+      </div>
+    </div>
+  </div> 
 @stop
 
