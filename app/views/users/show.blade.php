fba0dcb9e7b5ec71d859351f7af3e68cc6151bab
Christian Korndörfer <info@chko.org>
2014-07-15 22:56:42 +0200

    profile styles

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -6,24 +6,27 @@
 
 @section('body')
 
-  <div class="row">
-		<div class="col-sm-3"><!--left col-->
+  <div class="row profile">
+		<div class="col-md-3"><!--left col-->
 
       <ul class="list-group">
         <li class="list-group-item text-muted">
           @if($user->image)
-          <img src="{{ $user->image }}" alt="{{ $user->username }}" class="img-responsive profile-pic">
+            <img src="{{ $user->image }}" alt="{{ $user->username }}" class="img-responsive profile-pic">
           @else
-          <img src="/assets/PH_user_large.png" alt="{{ $user->username }}" class="img-responsive profile-pic">
+            <img src="/assets/PH_user_large.png" alt="{{ $user->username }}" class="img-responsive profile-pic">
+          @endif
+          @if(Auth::check() && Auth::user()->id == $user->id)
+            <a href="{{ URL::to('users/edit') }}" class="btn btn-default btn-sm edit-button" role="button"><i class="fa fa-gear fa-fw"></i> Edit Profile</a>
           @endif
         </li>
-        <li class="list-group-item text-right"><span class="pull-left"><strong>Username</strong></span> {{ $user->username }}</li>
+        <li class="list-group-item text-left"><strong>{{ $user->username }}</strong></li>
         @if($user->name)
-        <li class="list-group-item text-right"><span class="pull-left"><strong>Name</strong></span> {{ $user->name }}</li>
+        <li class="list-group-item text-left">{{ $user->name }}</li>
         @endif
-        <li class="list-group-item text-right"><span class="pull-left"><strong>Email</strong></span> {{ $user->email }}</li>
+        <li class="list-group-item text-left">{{ $user->email }}</li>
         @if($user->location)
-        <li class="list-group-item text-right"><span class="pull-left"><strong>Location</strong></span> {{ $user->location }}</li>
+        <li class="list-group-item text-left">{{ $user->location }}</li>
         @endif
       </ul>
 
@@ -31,61 +34,64 @@
 
       @if($user->website)
         <div class="panel panel-default">
-          <div class="panel-heading">Website <i class="fa fa-link fa-1x"></i></div>
+          <div class="panel-heading">
+            <h3 class="panel-title">Website <i class="fa fa-link fa-1x"></i></h3>
+          </div>
           <div class="panel-body"><a href="{{ $user->website }}" target="_blank">{{ $user->website }}</a></div>
         </div>
       @endif
 
       @if($user->description)
       <div class="panel panel-default">
-        <div class="panel-heading">Description</div>
+        <div class="panel-heading">
+          <h3 class="panel-title">Description</h3>
+        </div>
         <div class="panel-body">{{ $user->description }}</div>
       </div>
       @endif
-
-      @if(Auth::check() && Auth::user()->id == $user->id)
-        <a href="{{ URL::to('users/edit') }}" class="btn btn-default btn-sm" role="button" style="margin-right: 10px;"><i class="fa fa-gear fa-fw"></i> Edit Profile</a>
-      @endif
     </div>
 
-    <div class="col-sm-9">
-      <div class="page-header profile-user-name">
-        <h1>{{ $user->vinyls->count() }} vinyls</h1>
-      </div>
+    <div class="col-md-9">
 
-      <!-- Level progress -->
-      <div class="row">
-        <div class="col-xs-6 col-xs-offset-3 text-center well">
-          <h2>Level {{ $level }}</h2>
-          <div class="profile-user-rank">{{ $rank }}</div>
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Vinyl Count</h3>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <div class="progress">
-            <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="{{ $progress }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress }}%; min-width: 30px;">
-              {{ $progress }}%
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-xs-6 col-xs-offset-3 text-center">
+              <p class="h1">{{ $user->vinyls->count() }} Vinyls<p>
+              <hr>
+              <h3>Level {{ $level }}</h3>
+              <div class="profile-user-rank">{{ $rank }}</div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="progress">
+                <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="{{ $progress }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $progress }}%; min-width: 30px;">
+                  {{ $progress }}%
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-xs-4">
+              <strong>Lvl {{ $level }}</strong>
+            </div>
+            <div class="col-xs-4 text-center">
+              <span class="progress-count">{{ $vinyls->count() }}/{{ $nextLvlVinyls }} Vinyls</span>
+            </div>
+            <div class="col-xs-4">
+              <strong class="pull-right">Lvl {{ $level+1 }}</strong>
             </div>
           </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-4">
-          <strong>Lvl {{ $level }}</strong>
-        </div>
-        <div class="col-xs-4 text-center">
-          <span class="progress-count">{{ $vinyls->count() }}/{{ $nextLvlVinyls }} Vinyls</span>
-        </div>
-        <div class="col-xs-4">
-          <strong class="pull-right">Lvl {{ $level+1 }}</strong>
-        </div>
-      </div>
-
-      <hr>
+      </div><!-- end of progress -->
       
       <div class="panel panel-success">
         <div class="panel-heading">
-          <h3 class="panel-title">Whats it worth?</h3>
+          <h3 class="panel-title">Collection Value</h3>
         </div>
         <div class="panel-body">
           <div class="row">
