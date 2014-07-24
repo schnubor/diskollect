4f00c49a937c43f99f19358198b21e5d7393d7d2
Christian Korndörfer <info@chko.org>
2014-07-24 22:41:36 +0200

    style changed

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -25,9 +25,9 @@
           <img class="logo" src="/assets/logo.png" alt="Logo">
           <h2>Start sharing your vinyl collection today.</h2>
           <div class="buttons">
-            <div class="btn-group btn-group-lg">
-              <a class="btn btn-primary btn-lg" role="button" href="{{ URL::route('get-user-create') }}"><i class="fa fa-fw fa-edit"></i>Register</a>
-              <a class="btn btn-default btn-sm" role="button" href="{{ URL::route('get-signin') }}"><i class="fa fa-fw fa-sign-in"></i>Login</a>
+            <div class="btn-group btn-group-md">
+              <a class="btn btn-primary" role="button" href="{{ URL::route('get-user-create') }}"><i class="fa fa-fw fa-edit"></i>Register</a>
+              <a class="btn btn-default" role="button" href="{{ URL::route('get-signin') }}"><i class="fa fa-fw fa-sign-in"></i>Login</a>
             </div>
           </div>
         </div>
