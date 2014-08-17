389b26d2a5f73ff1d159640e82459e31569b94f3
schnubor <info@chko.org>
2014-08-17 21:15:23 +0200

    added JS

---
+++ app/assets/javascripts/vinyl_form.js
@@ -0,0 +1,14 @@
+$(function(){
+  console.log('DOM ready');
+
+  // Init Colorpicker
+  $('#colorpicker').spectrum();
+
+  // Update Artwork Preview
+  $('#vinyl-artwork-url').change(function(){
+    var value = $(this).val();
+    console.log(value);
+    $('#vinyl-artwork').attr('src', value);
+  });
+
+});
