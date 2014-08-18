1e7f4afcd70e2b0d998cbf68da5743f9c33a9cbf
Christian Korndörfer <info@chko.org>
2014-08-18 02:09:57 +0200

    genre graph ready

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -131,46 +131,27 @@
 @stop
 
 <?php
-  $genres = [];
+  $vinylGenres = [];
   foreach($user->vinyls as $vinyl){
-    $vinylGenre = explode(';',$vinyl->genre);
-    array_push($genres, $vinylGenre);
+    $genres = explode(';',$vinyl->genre);
+    foreach($genres as $genre){
+      array_push($vinylGenres, $genre);
+    }
   }
+  $genres = array_count_values($vinylGenres);
 ?>
 
 @section('scripts')
   <script>
     var data = [
-      {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-      },
-      {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-      },
-      {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-      },
-      {
-        value: 40,
-        color: "#949FB1",
-        highlight: "#A8B3C5",
-        label: "Grey"
-      },
-      {
-        value: 120,
-        color: "#4D5360",
-        highlight: "#616774",
-        label: "Dark Grey"
-      }
+      @foreach($genres as $key => $value)
+        {
+          value: {{ $value }},
+          color:"#F7464A",
+          highlight: "#FF5A5E",
+          label: "{{ $key }}"
+        },
+      @endforeach
     ];
     var options = {
       responsive: 'true'
