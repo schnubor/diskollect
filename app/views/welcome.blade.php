15ba66f33985278314cc19bbe4c10b6488485fbf
schnubor <info@chko.org>
2014-09-22 14:28:27 +0200

    style changes and added latest content

--- app/views/welcome.blade.php
+++ app/views/welcome.blade.php
@@ -11,36 +11,32 @@
         
         @if(Auth::check())
           <div class="welcome-message">
-              <h1><a href="{{ URL::route('get-user', Auth::user()->id ) }}">{{ Auth::user()->username }}</a></h1>
-              <h2>You got <a href="{{ URL::route('get-collection', Auth::user()->id ) }}"><strong>{{ Auth::user()->vinyls->count() }} Vinyls</strong></a> worth <a href="{{ URL::route('get-user', Auth::user()->id ) }}"><strong>{{ number_format(round(Auth::user()->vinyls->sum('price'), 2),2) }} {{ Auth::user()->currency }}</strong></a></h2>
+            <h1><a href="{{ URL::route('get-user', Auth::user()->id ) }}">{{ Auth::user()->username }}</a></h1>
+            <h2>You got <a href="{{ URL::route('get-collection', Auth::user()->id ) }}"><strong>{{ Auth::user()->vinyls->count() }} Vinyls</strong></a> worth <a href="{{ URL::route('get-user', Auth::user()->id ) }}"><strong>{{ number_format(round(Auth::user()->vinyls->sum('price'), 2),2) }} {{ Auth::user()->currency }}</strong></a></h2>
+            <div class="welcome-actions">
+              <div class="col-md-2 col-md-offset-3">
+                <a class="btn btn-lg btn-primary btn-block" role="button" href="{{ URL::route('get-search') }}"><i class="fa fa-fw fa-plus-circle"></i> Add Vinyl</a>
+              </div>
+              <div class="col-md-2">
+                <a class="btn btn-lg btn-primary btn-block" role="button" href="{{ URL::route('get-collection', Auth::user()->id ) }}"><i class="fa fa-fw fa-database"></i> Collection</a>
+              </div>
+              <div class="col-md-2">
+                <a class="btn btn-lg btn-primary btn-block" role="button" href="{{ URL::route('get-user', Auth::user()->id) }}"><i class="fa fa-fw fa-bar-chart-o"></i> Statistics</a>
+              </div>
+            </div>
           </div>
         @else
           <div class="welcome-message">
             <h1>Diskollect</h1>
             <h2>Tracking <strong>{{ $vinyls }}</strong> vinyls of <strong>{{ $users }}</strong> collectors.</h2>
           </div>
-        @endif
-      </div>
-    </div>
-
-    <div class="welcome-actions">
-      <div class="container">
-        @if(Auth::check())
-          <div class="col-md-2 col-md-offset-3">
-            <a class="btn btn-lg btn-primary btn-block" role="button" href="{{ URL::route('get-search') }}"><i class="fa fa-fw fa-plus-circle"></i> Add Vinyl</a>
-          </div>
-          <div class="col-md-2">
-            <a class="btn btn-lg btn-primary btn-block" role="button" href="{{ URL::route('get-collection', Auth::user()->id ) }}"><i class="fa fa-fw fa-database"></i> Collection</a>
-          </div>
-          <div class="col-md-2">
-            <a class="btn btn-lg btn-primary btn-block" role="button" href="{{ URL::route('get-user', Auth::user()->id) }}"><i class="fa fa-fw fa-bar-chart-o"></i> Statistics</a>
-          </div>
-        @else
-          <div class="col-md-3 col-md-offset-3">
-            <a class="btn btn-primary btn-lg btn-block" role="button" href="{{ URL::route('get-signin') }}"><i class="fa fa-fw fa-sign-in"></i>Login</a>
-          </div>
-          <div class="col-md-3">
-            <a class="btn btn-info btn-lg btn-block" role="button" href="{{ URL::route('get-user-create') }}"><i class="fa fa-fw fa-edit"></i>Register</a>
+          <div class="welcome-actions">
+            <div class="col-md-2 col-md-offset-4">
+              <a class="btn btn-primary btn-lg btn-block" role="button" href="{{ URL::route('get-signin') }}"><i class="fa fa-fw fa-sign-in"></i>Login</a>
+            </div>
+            <div class="col-md-2">
+              <a class="btn btn-default btn-lg btn-block" role="button" href="{{ URL::route('get-user-create') }}"><i class="fa fa-fw fa-edit"></i>Register</a>
+            </div>
           </div>
         @endif
       </div>
@@ -50,11 +46,27 @@
       <div class="container">
         @if(Auth::check())
           @if(Auth::user()->vinyls->count())
-            <h2>Why not listen to...</h2>
             <?php
               $randomVinyl = Auth::user()->vinyls()->orderBy(DB::raw('RAND()'))->first();
             ?>
-            <div class="col-md-4 col-md-offset-4">
+            <div class="col-md-8">
+              <p class="h2">Latest vinyls</p>
+              <hr>
+              <div class="row">
+                @foreach($latestVinyls as $vinyl)
+                  <div class="col-md-4 latest-vinyl">
+                    <a href="{{ URL::route('get-vinyl', $vinyl->id) }}"><img src="{{ $vinyl->artwork }}" alt="{{ $vinyl->artist.' - '.$vinyl->title }}" width="100%" class="img-thumbnail"></a>
+                    <p>
+                      <strong>{{ $vinyl->artist }}</strong><br>
+                      <span>{{ $vinyl->title }}</span>
+                    </p>
+                  </div>
+                @endforeach
+              </div>
+            </div>
+            <div class="col-md-4">
+              <p class="h2">Vinyl of the moment</p>
+              <hr>
               <a href="{{ URL::route('get-vinyl', $randomVinyl->id) }}"><img src="{{ $randomVinyl->artwork }}" class="img-thumbnail" alt="{{ $randomVinyl->artist.' - '.$randomVinyl->title }}" width="100%"></a>
               <p class="vinyl-info">
                 <strong>{{ $randomVinyl->artist }}</strong><br>
