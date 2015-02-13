e1cf811b6d236d920d91562c104ae71db0a1de7c
Christian Korndörfer <info@chko.org>
2015-02-13 14:48:07 +0100

    added homestead

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('christians-mbp','christians-imac','Christians-MacBook-Pro-2.local','Christians-MacBook-Pro.local','christians-mbp.fritz.box','christiansmbp2','Christians-MacBook-Pro-3.local')
+	'local' => array('homestead','christians-mbp','christians-imac','Christians-MacBook-Pro-2.local','Christians-MacBook-Pro.local','christians-mbp.fritz.box','christiansmbp2','Christians-MacBook-Pro-3.local')
 
 ));
 
